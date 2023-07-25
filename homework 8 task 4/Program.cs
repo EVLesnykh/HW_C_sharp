961d849c66f24473853d2c44e91f80b926495869
EKATERINA <evlesnykh@gmail.com>
2023-07-25 11:16:16 +1000

    add8.4

---
+++ homework 8 task 4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Трехмерный массив: ");
+int[,,] array = new int[2, 2, 2];
+RandomArray(array);
+PrintArray(array);
+
+void RandomArray(int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        array[i, j, k] = new Random().Next(0, 99);
+      }
+    }
+  }
+}
+
+void PrintArray(int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
